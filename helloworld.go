9f02df5ced4f34512430f10565e4aa2002bdf0da
40sherrin <fareedaskari42@gmail.com>
2022-05-15 09:45:50 +1000

    slice and make function

--- helloworld.go
+++ helloworld.go
@@ -5,6 +5,16 @@ import (
 )
 
 func main() {
-	cars := [4]string{"Volvo", "BMW", "Ford", "Mazda"}
-	fmt.Println(cars[3])
+	myArr := [6]int{1, 2, 3, 4, 5, 6}
+	mySlice := myArr[2:6]
+	
+	fmt.Printf("mySlice = %v\n", mySlice)
+	fmt.Printf("Length = %d\n", len(mySlice))
+	fmt.Printf("capacity = %d\n", cap(mySlice))
+
+	mySlice2 := make([]int, 5)
+
+	fmt.Printf("mySlice2 = %v\n", mySlice2)
+	fmt.Printf("length = %d\n", len(mySlice2))
+	fmt.Printf("capacity = %d\n", cap(mySlice2))
 }
