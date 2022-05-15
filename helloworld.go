9ceffffdfd94203ceab530b7b6c1a589bdd3a617
40sherrin <fareedaskari42@gmail.com>
2022-05-15 14:02:38 +1000

    finished

--- helloworld.go
+++ helloworld.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 )
 
-func factorial_recursion(x float64) (y float64) {
-	if x > 0 {
-		y = x * factorial_recursion(x-1)
-	} else {
-		y = 1
-	}
-	return
+type Person struct {
+	name   string
+	age    int
+	job    string
+	salary int
 }
 
 func main() {
-	fmt.Println(factorial_recursion(4))
+	var pers1 Person
+
+	pers1.name = "Fareed"
+	pers1.job = "Developer"
+	pers1.age = 22
+	pers1.salary = 120000
+
+	fmt.Println("Name: ", pers1.name)
+	fmt.Println("Age: ", pers1.age)
+	fmt.Println("Job: ", pers1.job)
+	fmt.Println("Salary: ", pers1.salary)
+
 }
